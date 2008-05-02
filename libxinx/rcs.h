b7624cdaed6dd6d137d1df7685228f09d8742da3
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2008-05-02 07:54:10 +0000

    Quelques commentaire sur RCS

--- libxinx/rcs.h
+++ libxinx/rcs.h
@@ -31,29 +31,58 @@
 #include <QList>
 #include <QDateTime>
 
+/*!
+ * Base class to create "revision control system" plugin. This plugin will create a derivated object when the project
+ * will be open, and destroy this object when the project is closed.
+ * 
+ * This object will be used along the project mode, to show to the user the \e status of a file, to \e update from repository,
+ * to \e commit to the repository, to \e add a file to the repository, or to \e remove a file from the repository.
+ */
 class XINX_EXPORT RCS : public QObject {
 	Q_OBJECT
 public:
+	/*! 
+	 * Create a RCS base object.
+	 * \param basePath This is the project path, used in the RCS as the base path of operations.
+	 */
 	RCS( const QString & basePath );
+	/*! Destroy the object */
 	virtual ~RCS();
 
+	/*! RCS state of a file on the disk */
 	enum rcsState {
-		Updated, LocallyModified, LocallyAdded, LocallyRemoved, NeedsCheckout, NeedPatch, UnresolvedConflict, FileHadConflictsOnMerge, Unknown
+		Updated, ///< The file is up to date, no operation to do. 
+		LocallyModified, ///< The file is modified on the disk but not in the repository. 
+		LocallyAdded, ///< The file is added to the repository 
+		LocallyRemoved, ///< The file is removed from the repository 
+		NeedsCheckout, ///< The file exists in the repository, but not in the disk 
+		NeedPatch, ///< Not used 
+		UnresolvedConflict, ///< The file has a conflict but not resolved 
+		FileHadConflictsOnMerge, ///< The file has a resolved conflict. 
+		Unknown ///< The state of the file is unknown, the file is not is the repository
 	};
+	/*! Information that can be returned in the log */
 	enum rcsLog {
-		LogError, LogNormal, LogConflict, LogLocallyModified, LogRemotlyModified, LogNotManaged, LogApplication
+		LogError, ///< The message is an error 
+		LogNormal, ///< The message has no attribute 
+		LogConflict, ///< The message is about conflicted files. 
+		LogLocallyModified, ///< The message is about modified files on the disk. 
+		LogRemotlyModified, ///< The message is about modified files on the repository.  
+		LogNotManaged, ///< The message has an unknown type. 
+		LogApplication ///< This is the application (XINX) who log the message.
 	};
+	/*! Operation that can be made on a file when commit is asked */
 	enum rcsOperation {
-		RemoveAndCommit, AddAndCommit, Commit, Nothing
+		RemoveAndCommit, ///< Remove the file and commit this change
+		AddAndCommit, ///< Add the file and commit it.
+		Commit, ///< Commit the file
+		Nothing ///< Do nothing
 	};
-	enum rcsInfos {
-		rcsVersions, rcsDate, rcsFileDate
-	};
-	
+	/*! Informations about a file  */
 	struct struct_rcs_infos {
-		rcsState state;
-		QString version;
-		QDateTime rcsDate;
+		rcsState state; ///< State of the file (see the enum \e rcsState)
+		QString version; ///< Version of the file on the disk
+		QDateTime rcsDate; ///< Date of the last modified time in the RCS
 	};
 	
 	typedef QPair<QString,rcsOperation> FileOperation;
