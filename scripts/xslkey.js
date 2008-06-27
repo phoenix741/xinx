53d8f5649f96a3727bea339bb6413a2195dc2f1d
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2008-06-27 13:58:13 +0000

    Correction sur le script xslkey

--- scripts/xslkey.js
+++ scripts/xslkey.js
@@ -12,30 +12,30 @@ obj.run = function()
 }; 
 
 obj.formatXslKey = function(text)
-{ 
-	// <xsl:value-of select="key('url-param', 'chp:Codmdl')/@value"/> 
-	while (text.indexOf("key('url-param'") != -1) 
-	{ 
-		var pos = text.indexOf("key('url-param'"); 
-
-		// Ce qui suit la virgule défini le nom du champ (à trimer) jusqu'à la 
-		// quote suivante 
-		var posV1 = text.indexOf(",", pos); 
-		var posQ1 = text.indexOf("'", posV1); 
-
-		var posQ2 = text.indexOf("'", posQ1 + 1); 
-		var field = text.substring(posQ1 + 1, posQ2); 
-
-		// Le guillemet suivant défini la position de fin de la chaîne 
-		// à remplacer 
-		var posE = text.indexOf("\"", posQ2); 
-
-		text = text.substring(0, pos) 
-				+ "/layout_data/application_data/temporaire/param[@name='" + field + "']/@value" 
-				+ text.substring(posE); 
+{
+	// <xsl:value-of select="key('url-param', 'chp:Codmdl')"/>
+	while (text.indexOf("key('url-param'") != -1)
+	{
+		var pos = text.indexOf("key('url-param'");
+ 
+		// Ce qui suit la virgule défini le nom du champ (à trimer) jusqu'à la
+		// quote suivante
+		var posV1 = text.indexOf(",", pos);
+		var posQ1 = text.indexOf("'", posV1);
+ 
+		var posQ2 = text.indexOf("'", posQ1 + 1);
+		var field = text.substring(posQ1 + 1, posQ2);
+ 
+		// La parenthèse suivante défini la position de fin de la chaîne
+		// à remplacer
+		var posE = text.indexOf(")", posQ2) + 1;
+ 
+		text = text.substring(0, pos)
+			+ "/layout_data/application_data/temporaire/param[@name='" + field + "']"
+			+ text.substring(posE);
 	}
-
-	return text; 
-} 
+ 
+	return text;
+}
 
 return obj; 
