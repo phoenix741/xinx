6ee1d312cb82a65df26fac6cdd4f6ec083721885
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2008-06-27 12:12:28 +0000

    Ajout des 'var'

--- scripts/tab.js
+++ scripts/tab.js
@@ -10,27 +10,27 @@ obj.run = function() {
 	var start = true;
 	var spaces = 0;
 
-	for( i = 0; i < text.length; i++ ) {
+	for( var i = 0; i < text.length; i++ ) {
 		if( start && (text[i] == ' ') ) {
-			spaces++;	
-			if( spaces == 4 ) {
-				result += '\t';
-				spaces = 0;
+			spaces++;
+			if( spaces == 4 ) { 
+				result += '\t'; 
+				spaces = 0; 
 			}
-		} else if( text[i] == '\n' ) {
-			start = true;
-			result += text[i];
-		} else {
-	    	for( j = 0; j < spaces ; j++ ) 
-				result += ' ';
-			spaces = 0;
-			start = false;
-			result += text[i];
-		}
-	}
+		} else if( text[i] == '\n' ) { 
+			start = true; 
+			result += text[i]; 
+		} else { 
+			for( var j = 0; j < spaces ; j++ ) 
+				result += ' '; 
+			spaces = 0; 
+			start = false; 
+			result += text[i]; 
+		} 
+	} 
 	
-	this.textEdit.plainText = result;
-	this.textEdit.document.modified = true;
-};
+	this.textEdit.plainText = result; 
+	this.textEdit.document.modified = true; 
+}; 
 
 return obj;
