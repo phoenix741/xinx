c272de1ad50ede4d377044c094537c7f101cd81e
Ulrich Van Den Hekke <ulrich.vdh@shadoware.org>
2010-08-04 12:34:55 +0000

    Update changelog

--- framework/core/version.h
+++ framework/core/version.h
@@ -1,7 +1,7 @@
 #define VERSION_MAJOR     0
 #define VERSION_MINOR     9
 #define VERSION_RELEASE   0
-#define VERSION_BUILD     8
+#define VERSION_BUILD     9
 
 #define str(s) #s
 #define xstr(s) str(s)
